7215e8b7a26f922540068f090435c55a36126b8c
oflorez <v-oflorez@expedia.com>
2020-09-15 16:07:00 -0400

    09. Add Todo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule} from '@ngrx/store-devtools';
 import { todoReducer } from './todo/todo.reducer';
 
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
@@ -28,6 +30,7 @@ import { environment } from '../environments/environment.prod';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    ReactiveFormsModule,
     StoreModule.forRoot({ todos: todoReducer }),
     StoreDevtoolsModule.instrument({
       maxAge: 25,
--- src/app/todo/todo-add/todo-add.component.ts
+++ src/app/todo/todo-add/todo-add.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../app.reducers';
+import * as fromTodo from '../todo.actions';
 
 @Component({
   selector: 'app-todo-add',
@@ -8,9 +12,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoAddComponent implements OnInit {
 
-  constructor() { }
+  textInput: FormControl;
+
+  constructor(private store: Store<AppState>) { }
 
   ngOnInit(): void {
+    this.textInput = new FormControl('', Validators.required);
+  }
+
+  addTodo(){
+    if (this.textInput.invalid){
+      return;
+    }
+
+    const action = new fromTodo.AddTodoAction(this.textInput.value);
+    this.store.dispatch(action);
+    this.textInput.setValue('');
   }
 
 }
