0629b73cab0277d25d330704ad88825c6a6a108d
oflorez <v-oflorez@expedia.com>
2020-09-17 14:19:58 -0400

    19. count pending todos

--- src/app/todo/todo-footer/todo-footer.component.ts
+++ src/app/todo/todo-footer/todo-footer.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import * as fromFilter from '../../filter/filter.actions';
 import { AppState } from '../../app.reducers';
 import { Store } from '@ngrx/store';
+import {Todo} from '../model/todo.model';
 
 @Component({
   selector: 'app-todo-footer',
@@ -11,6 +12,7 @@ import { Store } from '@ngrx/store';
 })
 export class TodoFooterComponent implements OnInit {
 
+  pendingTodos: number;
   validFilters: fromFilter.ValidFilters[] = ['all', 'completed', 'pending'];
   currentFilter: fromFilter.ValidFilters;
 
@@ -19,11 +21,16 @@ export class TodoFooterComponent implements OnInit {
   ngOnInit(): void {
     this.store.subscribe( state => {
       this.currentFilter = state.filter;
+      this.countPendingTodos(state.todos);
     });
   }
 
   changeFilter(filter: fromFilter.ValidFilters) {
     const action = new fromFilter.SetFilterAction(filter);
     this.store.dispatch(action);
+  }
+
+  countPendingTodos(todos: Todo[]){
+    this.pendingTodos = todos.filter( todo => !todo.completed ).length;
   }
 }
